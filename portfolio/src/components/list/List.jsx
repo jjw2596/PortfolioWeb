2c66730fa6948d80afde3dee9c08f153fd4fefc9
jjw2596 <jjw2596@naver.com>
2022-12-28 17:56:03 +0900

    cube position fix

--- portfolio/src/components/list/List.jsx
+++ portfolio/src/components/list/List.jsx
@@ -34,149 +34,159 @@ function List() {
     <div className='listContainer'>
         <ParticleBackground settings={settings} className='particle'/>
         <div className='listItem1'>
-          <div className='firstVrtLine'></div>
+          <div className='vrtLine1'></div>
           {/* <Cube className='firstCube'/> */}
-          <figure className='firstCube'>
-            <div class="face front">
-              <div></div>
-            </div>
+          <div className='fixedCube'>
+            <figure className='cube1'>
+              <div class="face front">
+                <div></div>
+              </div>
 
-            <div class="face top">
-              <div></div>
-            </div>
-            
-            <div class="face right">
-              <div></div>
-            </div>
+              <div class="face top">
+                <div></div>
+              </div>
+              
+              <div class="face right">
+                <div></div>
+              </div>
 
-            <div class="face left">
-              <div></div>
-            </div>
+              <div class="face left">
+                <div></div>
+              </div>
 
-            <div class="face bottom">
-              <div></div>
-            </div>
+              <div class="face bottom">
+                <div></div>
+              </div>
 
-            <div class="face back">
-              <div></div>
-            </div>
-          </figure>
+              <div class="face back">
+                <div></div>
+              </div>
+            </figure>
+          </div>
         </div>
         <div className='listItem2'>
-          <div className='scdVrtLine'></div>
+          <div className='vrtLine2'></div>
           {/* <Cube className='firstCube'/> */}
-          <figure className='scdCube'>
-            <div class="face front">
-              <div></div>
-            </div>
+          <div className='fixedCube'>
+            <figure className='cube2'>
+              <div class="face front">
+                <div></div>
+              </div>
 
-            <div class="face top">
-              <div></div>
-            </div>
-            
-            <div class="face right">
-              <div></div>
-            </div>
+              <div class="face top">
+                <div></div>
+              </div>
+              
+              <div class="face right">
+                <div></div>
+              </div>
 
-            <div class="face left">
-              <div></div>
-            </div>
+              <div class="face left">
+                <div></div>
+              </div>
 
-            <div class="face bottom">
-              <div></div>
-            </div>
+              <div class="face bottom">
+                <div></div>
+              </div>
 
-            <div class="face back">
-              <div></div>
-            </div>
-          </figure>
+              <div class="face back">
+                <div></div>
+              </div>
+            </figure>
+          </div>
         </div>
         <div className='listItem3'>
-          <div className='trdVrtLine'></div>
+          <div className='vrtLine3'></div>
           {/* <Cube className='firstCube'/> */}
-          <figure className='trdCube'>
-            <div class="face front">
-              <div></div>
-            </div>
+          <div className='fixedCube'>
+            <figure className='cube3'>
+              <div class="face front">
+                <div></div>
+              </div>
 
-            <div class="face top">
-              <div></div>
-            </div>
-            
-            <div class="face right">
-              <div></div>
-            </div>
+              <div class="face top">
+                <div></div>
+              </div>
+              
+              <div class="face right">
+                <div></div>
+              </div>
 
-            <div class="face left">
-              <div></div>
-            </div>
+              <div class="face left">
+                <div></div>
+              </div>
 
-            <div class="face bottom">
-              <div></div>
-            </div>
+              <div class="face bottom">
+                <div></div>
+              </div>
 
-            <div class="face back">
-              <div></div>
-            </div>
-          </figure>
+              <div class="face back">
+                <div></div>
+              </div>
+            </figure>
+          </div>
         </div>
         <div className='listItem4'>
-          <div className='frthVrtLine'></div>
+          <div className='vrtLine4'></div>
           {/* <Cube className='firstCube'/> */}
-          <figure className='frthCube'>
-            <div class="face front">
-              <div></div>
-            </div>
+          <div className='fixedCube'>
+            <figure className='cube4'>
+              <div class="face front">
+                <div></div>
+              </div>
 
-            <div class="face top">
-              <div></div>
-            </div>
-            
-            <div class="face right">
-              <div></div>
-            </div>
+              <div class="face top">
+                <div></div>
+              </div>
+              
+              <div class="face right">
+                <div></div>
+              </div>
 
-            <div class="face left">
-              <div></div>
-            </div>
+              <div class="face left">
+                <div></div>
+              </div>
 
-            <div class="face bottom">
-              <div></div>
-            </div>
+              <div class="face bottom">
+                <div></div>
+              </div>
 
-            <div class="face back">
-              <div></div>
-            </div>
-          </figure>
+              <div class="face back">
+                <div></div>
+              </div>
+            </figure>
+          </div>
         </div>
         <div className='listItem5'>
-          <div className='fithVrtLine'></div>
+          <div className='vrtLine5'></div>
           {/* <Cube className='firstCube'/> */}
-          <figure className='fithCube'>
-            <div class="face front">
-              <div></div>
-            </div>
+          <div className='fixedCube'>
+            <figure className='cube5'>
+              <div class="face front">
+                <div></div>
+              </div>
 
-            <div class="face top">
-              <div></div>
-            </div>
-            
-            <div class="face right">
-              <div></div>
-            </div>
+              <div class="face top">
+                <div></div>
+              </div>
+              
+              <div class="face right">
+                <div></div>
+              </div>
 
-            <div class="face left">
-              <div></div>
-            </div>
+              <div class="face left">
+                <div></div>
+              </div>
 
-            <div class="face bottom">
-              <div></div>
-            </div>
+              <div class="face bottom">
+                <div></div>
+              </div>
 
-            <div class="face back">
-              <div></div>
-            </div>
-          </figure>
+              <div class="face back">
+                <div></div>
+              </div>
+            </figure>
+          </div>
         </div>
         
     </div>
